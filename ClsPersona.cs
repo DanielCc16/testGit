97f1e20f3e58251e2883b2640fcedae747b861ed
DanielCc16 <danieltf17@outlook.es>
2024-11-19 15:20:17 -0500

    test-01

---
+++ ClsPersona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bancoPoo
+{
+    class ClsPersona
+    {
+        private int idPersona;
+        private int dni;
+        private string nombre;
+        private string apellidoPat;
+        private string apellidoMat;
+
+
+    }
+}
